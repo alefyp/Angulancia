38e757b2d31e447c148fd5bec433706a8241a679
alefyp <alejndraprz@gmail.com>
2021-02-04 14:15:17 -0500

    landing component and scss globals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  template: `
-  <div>
-    <h1>{{appTitle}}</h1>
-    <div>My first Component</div>
-  </div>`
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
   appTitle: string = 'Angulancia';
+  appDescription: string = 'Emergency room patient management';
 }
