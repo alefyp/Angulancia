2e1f817c4f3f951ad90608fc14b0009041e8ce9a
alefyp <alejndraprz@gmail.com>
2021-02-04 17:09:51 -0500

    routing paths update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { RegisterComponent } from  './register/register.component';
+import { StatusComponent } from './status/status.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'status', component: StatusComponent },
+  { path: '**', component: NotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
